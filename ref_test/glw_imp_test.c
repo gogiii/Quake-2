7f483f8201086c11896b61b21c862de3ec6d51f7
gogiii <gvm@nm.ru>
2020-05-13 15:40:00 +0300

    some path fixed, solution changes added

--- ref_test/glw_imp_test.c
+++ ref_test/glw_imp_test.c
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include <assert.h>
 #include <windows.h>
-#include "../ref_gl/gl_local.h"
+#include "gl_local.h"
 #include "glw_win_test.h"
 #include "../win32/winquake.h"
 
